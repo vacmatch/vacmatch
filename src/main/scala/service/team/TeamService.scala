6867ee23bf0f1599e1d4f99a2da84dcf52fb8aca
Santiago Saavedra <santiagosaavedra@gmail.com>
2014-11-25 17:20:25 +0100

    Whitespace removal

--- src/main/scala/service/team/TeamService.scala
+++ src/main/scala/service/team/TeamService.scala
@@ -10,23 +10,20 @@ import main.scala.model.competition.Competition
 trait TeamService {
 
   def findTeamsByFederationId(fedId: Long, startIndex: Int, count: Int): List[Team]
-  
+
   def findByTeamId(teamId: Long): Team
-  
+
   def createTeam(teamName: String, fundationalDate: Calendar): Team
 
   def modifyTeamName(teamId: Long, newName: String): Team
-  
+
   def modifyTeamDate(teamId: Long, newDate: Calendar): Team
-  
+
   def modifyTeamSponsors(teamId: Long, newSponsors: List[String]): Team
-  
+
   def modifyStaff(teamId: Long, newStaffList: List[Staff]): Team
-  
+
   def modifyCompetitions(teamId: Long, newCompetitionList: List[Competition]): Team
 
   def getNumberByFederationId(fedId: Long): Long
 }
-
-
-
