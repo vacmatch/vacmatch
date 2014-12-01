7e99fb596af02b331c5bccdf6341440026057699
Santiago Saavedra <santiagosaavedra@gmail.com>
2014-12-01 13:29:26 +0100

    Implement findAllByFedId (leagues)

--- src/main/scala/model/competition/LeagueDao.scala
+++ src/main/scala/model/competition/LeagueDao.scala
@@ -3,5 +3,6 @@ package main.scala.model.competition
 import main.scala.model.generic.GenericDao
 
 trait LeagueDao extends GenericDao[League, java.lang.Long] {
+  def findAllByFedId(fedId: Long): Seq[League]
   def findBySlug(fedId: Long, slug: String): Option[League]
 }
--- src/main/scala/model/competition/LeagueDaoJpa.scala
+++ src/main/scala/model/competition/LeagueDaoJpa.scala
@@ -4,9 +4,29 @@ import main.scala.model.generic.GenericDaoHibernate
 import org.springframework.stereotype.Repository
 import javax.persistence.criteria.Predicate
 import javax.persistence.criteria.Expression
+import scala.collection.JavaConverters._
 
 @Repository("leagueDao")
 class LeagueDaoJpa extends GenericDaoHibernate[League, java.lang.Long](classOf[League]) with LeagueDao {
+
+  def findAllByFedId(fedId: Long): Seq[League] = {
+    val cb = getEntityManager.getCriteriaBuilder
+    var criteria = cb createQuery (classOf[League])
+
+    val root = criteria from (classOf[League])
+    val cond =
+      cb.and(
+        Array(
+          cb.equal(root get ("fedId"), fedId): Predicate
+        ):_*
+      )
+
+    criteria = criteria select root where (Array(cond):_*)
+
+    val q = getEntityManager createQuery criteria
+
+    return q.getResultList().asScala
+  }
 
   def findBySlug(fedId: Long, slug: String): Option[League] = {
     val cb = getEntityManager.getCriteriaBuilder
