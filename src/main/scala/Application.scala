b1f8a6919642709d3ea54c1cef7177b5d40aaf4b
Santiago Saavedra <santiagosaavedra@gmail.com>
2014-12-11 16:59:43 +0100

    Add springmvc-router support to spring-boot

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,18 +1,33 @@
 package main.scala
 
-import org.springframework.context.annotation.Configuration
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.ImportResource
+import java.util.ArrayList
+import org.resthub.web.springmvc.router.RouterConfigurationSupport
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.context.embedded.FilterRegistrationBean
+import org.springframework.context.annotation.{ Bean, ComponentScan, Configuration, Import }
+import scala.collection.JavaConverters._
+
+@Configuration
+@ComponentScan(basePackages = Array("main.scala")) // You should not use the @EnableWebMvc annotation
+class WebAppConfig() extends RouterConfigurationSupport {
+
+  @Override
+  def listRouteFiles(): ArrayList[String] = {
+    val routeFiles = new ArrayList[String];
+    routeFiles.add("classpath:/routes.conf");
+    return routeFiles;
+  }
+}
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-class Application 
-
+@Import(Array(classOf[WebAppConfig]))
+class Application {
+}
 
 object Application extends App {
-	SpringApplication.run(classOf[Application]);
+  SpringApplication.run(classOf[Application]);
 }
-  
+
