aba83efc13a22da9e1c40ac466bbca6f02942634
Pablo Castro <castrinho8@gmail.com>
2014-12-16 15:49:28 +0100

    Created StaffController and routes to list staff by federation Id

--- src/main/scala/controllers/staff/StaffController.scala
+++ src/main/scala/controllers/staff/StaffController.scala
@@ -11,27 +11,36 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.bind.annotation.ModelAttribute
+import main.scala.service.personal.AddressService
+import main.scala.model.personal.Address
+import java.util.Calendar
 
 @Controller
-@RequestMapping(value=Array("/staff"))
+@SessionAttributes(Array("fedId"))
 class StaffController {
   
   @Autowired
   var staffService: StaffService = _
 
   @Autowired
-  var playerService: PlayerService = _
+  var addressService: AddressService = _
+  
+  def list(@ModelAttribute("fedId") fedId: Long) = {
+    var staffList: Seq[Staff] = staffService.findAllByFederationId(fedId)
     
-  @RequestMapping(value=Array("/"), method=Array(RequestMethod.GET))
-  def showStaffList() = {
+    var mav: ModelAndView = new ModelAndView("staff/list")
+    mav.addObject("staffList", staffList)
+    mav.addObject("fedId", fedId)
+    mav
+  }
+  
+  def show() = {
     ""
   }
   
-  @RequestMapping(value=Array("/{staffId}"), method=Array(RequestMethod.GET))
-  def showStaff() = {
-    ""
-  }
-
 }
 
 
