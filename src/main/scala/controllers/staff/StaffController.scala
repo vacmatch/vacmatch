8cee43e14314226f2af68c0f99415fb16d87d9ec
Pablo Castro <castrinho8@gmail.com>
2014-12-26 17:21:11 +0100

    Added controllers and views of CreateStaff funcitionality with i18n

--- src/main/scala/controllers/staff/StaffController.scala
+++ src/main/scala/controllers/staff/StaffController.scala
@@ -18,22 +18,60 @@ import main.scala.service.personal.AddressService
 import main.scala.model.personal.Address
 import java.util.Calendar
 import javax.servlet.http.HttpSession
+import main.scala.controllers.utils.UrlGrabber
+import javax.validation.Valid
+import org.springframework.validation.BindingResult
+import main.scala.model.personal.AddressSpain
+import main.scala.service.personal.AddressSpainService
+import main.scala.model.generic.exceptions.InstanceNotFoundException
+import main.scala.model.generic.exceptions.InstanceNotFoundException
+import scala.beans.BeanProperty
 
 @Controller
 @SessionAttributes(Array("fedId"))
-class StaffController {
+class StaffController extends UrlGrabber {
   
   @Autowired
   var staffService: StaffService = _
 
   @Autowired
-  var addressService: AddressService = _
+  var addressSpainService: AddressSpainService = _
   
+  class PostStaff {
+    @BeanProperty
+    var stName: String = _
+    @BeanProperty
+    var stSurnames: String = _
+    @BeanProperty
+    var stEmail: String = _
+    @BeanProperty
+    var stTelephones: String = _
+    @BeanProperty
+    var addRoad: String = _
+    @BeanProperty
+    var addNumber: String = _
+    @BeanProperty
+    var addFlat: String = _
+    @BeanProperty
+    var addPostCode: Int = _
+    @BeanProperty
+    var addLocality: String = _
+    @BeanProperty
+    var addProvince: String = _
+    @BeanProperty
+    var addCountry: String = _
+    @BeanProperty
+    var stNif: String = _
+    @BeanProperty
+    var stBirth: Calendar = _
+  }
+
   def list(
+      @RequestParam("fedId") fedId: Long,
       session: HttpSession) = {
     
     //Get fedId from session
-    var fedId: Long = session.getAttribute("fedId").asInstanceOf[Long]
+    //var fedId: Long = session.getAttribute("fedId").asInstanceOf[Long]
     
     var staffList: Seq[Staff] = staffService.findAllByFederationId(fedId)
     
@@ -44,10 +82,11 @@ class StaffController {
   
   def showStaff(
       @PathVariable("staffId") staffId: Long, 
+      @RequestParam("fedId") fedId: Long,
       session: HttpSession)= {
     
     //Get fedId from session
-    var fedId: Long = session.getAttribute("fedId").asInstanceOf[Long]
+    //var fedId: Long = session.getAttribute("fedId").asInstanceOf[Long]
     
     var staffOpt: Option[Staff] = staffService.findByStaffId(staffId, fedId)
     
@@ -61,6 +100,59 @@ class StaffController {
     }
   }
   
+  def create(
+      @RequestParam("fedId") fedId: Long,
+      session: HttpSession) = {
+
+    //Get fedId from session
+    //var fedId: Long = session.getAttribute("fedId").asInstanceOf[Long]
+    
+    var submitUrl: String = getUrl("StaffController.createPost")
+    var submitMethod: String = "POST"
+    var receiverStaff: PostStaff = new PostStaff
+
+    var mav: ModelAndView = new ModelAndView("staff/create")
+    mav.addObject("staff", receiverStaff)
+    mav.addObject("fedId", fedId)
+    mav.addObject("submitUrl", submitUrl)
+    mav.addObject("submitMethod", submitMethod)
+    mav
+  }
+
+  def createPost(
+      @RequestParam("fedId") fedId: Long,
+      @Valid postStaff: PostStaff,
+      result: BindingResult) = {
+
+    //Get fedId from session
+    //var fedId: Long = session.getAttribute("fedId").asInstanceOf[Long]
+    
+    var mav: ModelAndView = new ModelAndView("/staff")
+    
+    if(result.hasErrors()){
+      mav = new ModelAndView("staff/new")
+    }else{
+      var staffSurnames: Seq[String] = staffService.getSurnamesFromString(postStaff.stSurnames)
+      var staffTelephones: Seq[String] = staffService.getTelephonesFromString(postStaff.stTelephones)
+      var staffAddress: AddressSpain = addressSpainService.createAddress(
+          postStaff.addRoad, postStaff.addNumber, postStaff.addFlat,
+          postStaff.addPostCode, postStaff.addLocality, postStaff.addProvince,
+          postStaff.addCountry)
+      
+      try {
+        var staff: Staff = staffService.createStaff(postStaff.stName, staffSurnames,
+          postStaff.stEmail, staffTelephones, staffAddress, postStaff.stNif,
+          postStaff.stBirth, fedId)
+          
+        mav = new ModelAndView("redirect:/staff/" + staff.staffId)
+      } catch {
+        //Federation not found
+        case e: InstanceNotFoundException => mav = new ModelAndView("redirect:/federation/notfound")
+      }
+    }
+    
+    mav
+  }
 }
 
 
