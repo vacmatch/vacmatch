cc18fbde45fe7a4d9ca276b9985a5fd441d2e45f
Santiago Saavedra <ssaavedra@vacmatch.com>
2015-07-20 23:10:26 +0200

    Sample usage of Future API with Spring to check whether it works
    Change-Id: I23486ed1410eec88e834c482d48adbdb40b534f1

--- src/main/scala/com/vac/manager/controllers/ApplicationController.scala
+++ src/main/scala/com/vac/manager/controllers/ApplicationController.scala
@@ -3,15 +3,17 @@ package com.vac.manager.controllers
 import org.springframework.stereotype.Controller
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.bind.annotation.RequestMapping
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Controller
 @RequestMapping(Array("/"))
 class ApplicationController() {
 
   @RequestMapping(Array("/"))
-  def index = {
+  def index: Future[ModelAndView] = {
     var mav: ModelAndView = new ModelAndView("home");
-    mav
+    Future.successful(mav)
   }
 
 }
